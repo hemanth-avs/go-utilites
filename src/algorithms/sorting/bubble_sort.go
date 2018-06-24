53a862e84694dbd518f04bd7c6392956a2a6c69d
hemanth.avs <hemanth.avs@us.ibm.com>
2018-06-24 03:00:56 -0500

    Go Formatting

--- src/algorithms/sorting/bubble_sort.go
+++ src/algorithms/sorting/bubble_sort.go
@@ -2,17 +2,17 @@ package sorting
 
 func Bubblesort(input []int) []int {
 
-  input_copy := make([]int, len(input))
-  copy(input_copy, input)
+	input_copy := make([]int, len(input))
+	copy(input_copy, input)
 
-  for outerloop:=0; outerloop<len(input_copy); outerloop++ {
-    for innerloop:=0; innerloop<len(input_copy)-1-outerloop; innerloop++ {
-      if input_copy[innerloop+1] < input_copy[innerloop] {
-        temp := input_copy[innerloop]
-        input_copy[innerloop] = input_copy[innerloop+1]
-        input_copy[innerloop+1] = temp
-      }
-    }
-  }
-  return input_copy
+	for outerloop := 0; outerloop < len(input_copy); outerloop++ {
+		for innerloop := 0; innerloop < len(input_copy)-1-outerloop; innerloop++ {
+			if input_copy[innerloop+1] < input_copy[innerloop] {
+				temp := input_copy[innerloop]
+				input_copy[innerloop] = input_copy[innerloop+1]
+				input_copy[innerloop+1] = temp
+			}
+		}
+	}
+	return input_copy
 }
--- src/algorithms/sorting/bubble_sort_test.go
+++ src/algorithms/sorting/bubble_sort_test.go
@@ -1,38 +1,38 @@
 package sorting
 
 import (
-    "testing"
-    "reflect"
+	"reflect"
+	"testing"
 )
 
-func TestBubblesort(t *testing.T){
+func TestBubblesort(t *testing.T) {
 
-  input := []int{1,2,3,2,1}
-  sorted_input := []int{1,1,2,2,3}
+	input := []int{1, 2, 3, 2, 1}
+	sorted_input := []int{1, 1, 2, 2, 3}
 
-  if !reflect.DeepEqual(Bubblesort(input), sorted_input) {
-    t.Error("Not Sorted")
-  }
+	if !reflect.DeepEqual(Bubblesort(input), sorted_input) {
+		t.Error("Not Sorted")
+	}
 
-  input = []int{9,8,7,6,5,4,3,2,1,0,1,2,3,4,5,6,7,8,9}
-  sorted_input = []int{0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9}
+	input = []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	sorted_input = []int{0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9}
 
-  if !reflect.DeepEqual(Bubblesort(input), sorted_input) {
-    t.Error("Not Sorted")
-  }
+	if !reflect.DeepEqual(Bubblesort(input), sorted_input) {
+		t.Error("Not Sorted")
+	}
 
-  input = []int{1}
-  sorted_input = []int{1}
+	input = []int{1}
+	sorted_input = []int{1}
 
-  if !reflect.DeepEqual(Bubblesort(input), sorted_input) {
-    t.Error("Not Sorted")
-  }
+	if !reflect.DeepEqual(Bubblesort(input), sorted_input) {
+		t.Error("Not Sorted")
+	}
 
-  input = []int{}
-  sorted_input = []int{}
+	input = []int{}
+	sorted_input = []int{}
 
-  if !reflect.DeepEqual(Bubblesort(input), sorted_input) {
-    t.Error("Not Sorted")
-  }
+	if !reflect.DeepEqual(Bubblesort(input), sorted_input) {
+		t.Error("Not Sorted")
+	}
 
 }
