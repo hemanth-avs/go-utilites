2d50c95893a07ab64ee3f5c2f19831259a4230ad
hemanth.avs <hemanth.avs@us.ibm.com>
2018-06-26 00:17:11 -0500

    updated test function

--- src/algorithms/sorting/sort_test_helper.go
+++ src/algorithms/sorting/sort_test_helper.go
@@ -7,46 +7,35 @@ import (
 
 func SortTester(sort SortAlgorithm, t *testing.T) {
 
+	checkSortResults := func(t *testing.T, sort SortAlgorithm, testInput []int, expectedOutput []int) {
+
+		if !reflect.DeepEqual(sort.Sort(testInput), expectedOutput) {
+			t.Error("Sort Failed")
+		}
+	}
+
 	input := []int{1, 2, 3, 2, 1}
 	sorted_input := []int{1, 1, 2, 2, 3}
-
-	if !reflect.DeepEqual(sort.Sort(input), sorted_input) {
-		t.Error("Not Sorted")
-	}
+	checkSortResults(t, sort, input, sorted_input)
 
 	input = []int{6, 1, 2, 3, 4, 5}
 	sorted_input = []int{1, 2, 3, 4, 5, 6}
-
-	if !reflect.DeepEqual(sort.Sort(input), sorted_input) {
-		t.Error("Not Sorted")
-	}
+	checkSortResults(t, sort, input, sorted_input)
 
 	input = []int{1, 2, 3, 4, 5, 6, 0}
 	sorted_input = []int{0, 1, 2, 3, 4, 5, 6}
-
-	if !reflect.DeepEqual(sort.Sort(input), sorted_input) {
-		t.Error("Not Sorted")
-	}
+	checkSortResults(t, sort, input, sorted_input)
 
 	input = []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	sorted_input = []int{0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9}
-
-	if !reflect.DeepEqual(sort.Sort(input), sorted_input) {
-		t.Error("Not Sorted")
-	}
+	checkSortResults(t, sort, input, sorted_input)
 
 	input = []int{1}
 	sorted_input = []int{1}
-
-	if !reflect.DeepEqual(sort.Sort(input), sorted_input) {
-		t.Error("Not Sorted")
-	}
+	checkSortResults(t, sort, input, sorted_input)
 
 	input = []int{}
 	sorted_input = []int{}
-
-	if !reflect.DeepEqual(sort.Sort(input), sorted_input) {
-		t.Error("Not Sorted")
-	}
+	checkSortResults(t, sort, input, sorted_input)
 
 }
