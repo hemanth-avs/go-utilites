2297bafe1be6ba5bc0324270a2ffb8e093844acf
hemanth.avs <hemanth.avs@us.ibm.com>
2018-06-27 23:33:38 -0500

    Updated Testcases

--- src/algorithms/sorting/sort_test_helper.go
+++ src/algorithms/sorting/sort_test_helper.go
@@ -14,28 +14,21 @@ func SortTester(sort SortAlgorithm, t *testing.T) {
 		}
 	}
 
-	input := []int{1, 2, 3, 2, 1, 0}
-	sorted_input := []int{1, 1, 2, 2, 3}
-	checkSortResults(t, sort, input, sorted_input)
+	testCases := []struct {
+		name     string
+		given    []int
+		expected []int
+	}{
+		{"testcase1", []int{1, 2, 3, 2, 1}, []int{1, 1, 2, 2, 3}},
+		{"testcase2", []int{6, 1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5, 6}},
+		{"testcase3", []int{1, 2, 3, 4, 5, 6, 0}, []int{0, 1, 2, 3, 4, 5, 6}},
+		{"testcase4", []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, []int{0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9}},
+		{"testcase5", []int{1}, []int{1}},
+		{"testcase6", []int{}, []int{}},
+	}
 
-	input = []int{6, 1, 2, 3, 4, 5}
-	sorted_input = []int{1, 2, 3, 4, 5, 6}
-	checkSortResults(t, sort, input, sorted_input)
-
-	input = []int{1, 2, 3, 4, 5, 6, 0}
-	sorted_input = []int{0, 1, 2, 3, 4, 5, 6}
-	checkSortResults(t, sort, input, sorted_input)
-
-	input = []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	sorted_input = []int{0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9}
-	checkSortResults(t, sort, input, sorted_input)
-
-	input = []int{1}
-	sorted_input = []int{1}
-	checkSortResults(t, sort, input, sorted_input)
-
-	input = []int{}
-	sorted_input = []int{}
-	checkSortResults(t, sort, input, sorted_input)
+	for _, tc := range testCases {
+		checkSortResults(t, sort, tc.given, tc.expected)
+	}
 
 }
